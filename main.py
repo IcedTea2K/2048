833f6c75682c358746d8950669bf412eac66c4d2
Minh Au <auduongminh2004@gmail.com>
2022-10-17 23:34:12 -0700

    add properties for cell drawing. Still need to fix the position

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from ctypes.wintypes import RGB
 import sys
+from turtle import Screen, color
 import pygame as pg
 
 def main():
@@ -13,6 +15,11 @@ def main():
     GRID_RECT = pg.Rect((0, 0), GRID_SIZE)
     GRID_RECT.center = GRID_POS
 
+    CELL_SIZE = 106, 106
+    CELL_COLOR = 238,228,218,0.35
+    PADDING = (GRID_SIZE[0] - CELL_SIZE[0])/5
+    CELL_RECTS = [[pg.Rect((x*CELL_SIZE[0] + PADDING, y*CELL_SIZE[0] + PADDING), CELL_SIZE) for y in range(0,4)] for x in range(0,4)]
+     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -20,9 +27,13 @@ def main():
         
         SCREEN.fill(BGCOLOR) # reset screen
         pg.draw.rect(SCREEN, GRID_BGCOLOR, GRID_RECT)
+        drawCells(SCREEN, CELL_COLOR, CELL_RECTS)
         pg.display.flip()
 
+def drawCells(screen:pg.Surface, color:tuple, rects: list[list[pg.Rect]]) -> None:
+    for x in rects:
+        for y in x:
+            pg.draw.rect(screen, color, y)
 
 if __name__ == '__main__':
     main()
-
