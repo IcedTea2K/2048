62e570af3ed14b6592dd7df9bc1ee5f13c022518
Minh Au <auduongminh2004@gmail.com>
2022-10-24 23:01:04 -0700

    create an outline for GameMaster class template

---
+++ GameMaster.py
@@ -0,0 +1,15 @@
+class GameMaster:
+    def __init__(self, screen) -> None:
+        self.screen = screen
+    
+    def moveSquare():
+
+        pass
+    def combineSquare():
+        pass
+
+    def spawnSquare():
+        pass
+
+    def renderSquare():
+        pass
