9ca65e03d8cda67f2b7e5d58aa53360f03d83dc4
Minh Au <auduongminh2004@gmail.com>
2022-12-29 15:09:35 -0600

    make constants global

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import sys
 import pygame as pg
 
+SIZE = WIDTH, HEIGHT = 550, 800
+SCREEN = pg.display.set_mode(SIZE)
+BGCOLOR = 250,248,239
+
+GRID_BGCOLOR = 187,173,160
+GRID_SIZE = 500, 500
+GRID_POS = (WIDTH /2, HEIGHT - GRID_SIZE[0]/2 - 30)
+GRID_RECT = pg.Rect((0, 0), GRID_SIZE)
+GRID_RECT.center = GRID_POS
+
+CELL_SIZE = 106, 106
+CELL_COLOR = 238,228,218,100
+PADDING = (GRID_SIZE[0] - CELL_SIZE[0]*4)/5
+CELL_RECTS = [[pg.Rect((x*CELL_SIZE[0] + PADDING*(x+1) + GRID_RECT.topleft[0], y*CELL_SIZE[1] + PADDING*(y+1) + GRID_RECT.topleft[1]), CELL_SIZE) \
+    for y in range(0,4)] for x in range(0,4)]
+CELL_SURFACE = pg.Surface(CELL_SIZE, pg.SRCALPHA) 
+CELL_SURFACE.fill(CELL_COLOR) # set color of the cell
+
 def main():
     pg.init()
-    SIZE = WIDTH, HEIGHT = 550, 800
-    SCREEN = pg.display.set_mode(SIZE)
-    BGCOLOR = 250,248,239
-
-    GRID_BGCOLOR = 187,173,160
-    GRID_SIZE = 500, 500
-    GRID_POS = (WIDTH /2, HEIGHT - GRID_SIZE[0]/2 - 30)
-    GRID_RECT = pg.Rect((0, 0), GRID_SIZE)
-    GRID_RECT.center = GRID_POS
-
-    CELL_SIZE = 106, 106
-    CELL_COLOR = 238,228,218,100
-    PADDING = (GRID_SIZE[0] - CELL_SIZE[0]*4)/5
-    CELL_RECTS = [[pg.Rect((x*CELL_SIZE[0] + PADDING*(x+1) + GRID_RECT.topleft[0], y*CELL_SIZE[1] + PADDING*(y+1) + GRID_RECT.topleft[1]), CELL_SIZE) \
-        for y in range(0,4)] for x in range(0,4)]
-    CELL_SURFACE = pg.Surface(CELL_SIZE, pg.SRCALPHA) 
-    CELL_SURFACE.fill(CELL_COLOR) # set color of the cell
-
     allSquares = [] # list of all the squares in the game
 
     while True:
