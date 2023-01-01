e191aa9db30cf43b0a4a79eab69d12745a5c9861
Minh Au <auduongminh2004@gmail.com>
2023-01-01 13:47:56 -0600

    organize the functions and clean up the code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame as pg
 import random
 from Square import *
 
-########## Graphics ##########
+#################### Graphics ####################
 SIZE = WIDTH, HEIGHT = 550, 800
 SCREEN = pg.display.set_mode(SIZE)
 BGCOLOR = 250,248,239
@@ -34,7 +34,7 @@ GAMEOVER_TXT_SIZE = 100
 
 TITLE_TXT_SIZE = 100
 
-########## CELLS & SQUARES SPECS ##########
+#################### CELLS & SQUARES SPECS ####################
 CELL_SIZE = 106, 106
 CELL_COLOR = 238,228,218,100
 PADDING = (GRID_SIZE[0] - CELL_SIZE[0]*4)/5
@@ -55,95 +55,13 @@ SQUARE_TXT_SIZE_LARGE_NUM = 50
 SQUARE_TXT_COLOR_SMALL_NUM = (119, 110, 101)
 SQUARE_TXT_COLOR_LARGE_NUM = (255, 255, 255)
 
-def main():
-    pg.init()
-    pg.font.init()
-    
-    global allSquares
-    global occupiedCells
-    allSquares = []
-    occupiedCells = {}
-    spawnSquare() 
-    spawnSquare()  
-    hasMoved = False
 
-    global highScore
-    global currScore
-    highScore = 0
-    currScore = 0
-    with open('high_score.txt', 'a+') as f:
-        f.seek(0)
-        m = f.read()
-        if m != '':
-            highScore = int(m)
-        else:
-            highScore = 0
-
-    while True:
-        if currScore > highScore:
-            highScore = currScore
-        
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                with open('high_score.txt', 'w') as f:
-                    f.write(str(highScore)) 
-                sys.exit()
-            elif event.type == pg.KEYUP:
-                if event.key == pg.K_UP:
-                    hasMoved = moveSquares((0,-1))
-                elif event.key == pg.K_RIGHT:
-                    hasMoved = moveSquares((1, 0))
-                elif event.key == pg.K_LEFT:
-                    hasMoved = moveSquares((-1, 0))
-                elif event.key == pg.K_DOWN:
-                    hasMoved = moveSquares((0, 1))
-            elif event.type == pg.MOUSEBUTTONDOWN:
-                if NEW_GAME_RECT.collidepoint(pg.mouse.get_pos()):
-                    allSquares = []
-                    occupiedCells = {}
-                    spawnSquare() 
-                    spawnSquare()
-                    currScore = 0
-        SCREEN.fill(BGCOLOR) # reset screen
-        # Draw background
-        pg.draw.rect(SCREEN, GRID_BGCOLOR, GRID_RECT) # draw grid background
-        drawCells(CELL_SURFACE, CELL_RECTS) # draw each cells of grid
-        hasMoved = updateSquares(hasMoved)
-        drawScoreBox()
-        drawButton()
-        drawTitle()
-        
-        
-        # draw squares
-        renderSquares()
-        if len(occupiedCells) == 16 and isGameOver():
-            gameOver()
-        pg.display.flip()
-
+#################### Functions for Graphics ####################
 def drawTitle() -> None:
     titleWriter = pg.font.Font(None, TITLE_TXT_SIZE)
     titleSurf = titleWriter.render('2048', True,(117, 110, 102))
     titleRect = titleSurf.get_rect(center=(150, 120))
     SCREEN.blit(titleSurf, titleRect)
-
-def isGameOver() -> bool:
-    for y in range(4):
-        for x in range(4):
-            if (occupiedCells.get(((x+1), y)) is not None and \
-                occupiedCells[(x+1, y)].getNum() == occupiedCells[(x, y)].getNum()) or \
-                    (occupiedCells.get((x, y+1)) is not None and \
-                        occupiedCells[(x, y+1)].getNum() == occupiedCells[(x, y)].getNum()):
-                return False
-    return True
-
-def gameOver() -> None:
-    writer = pg.font.Font(None, GAMEOVER_TXT_SIZE)
-    gameOverTxtSurf = writer.render('Game Over', True, (255, 255, 255))
-    gameOverTxtRect = gameOverTxtSurf.get_rect(center=GRID_RECT.center)
-
-    
-    SCREEN.blit(GAMEOVER_SURF, GRID_RECT)
-    SCREEN.blit(gameOverTxtSurf, gameOverTxtRect)
 
 def drawScoreBox() -> None:
     writer = pg.font.Font(None, SCORE_TXT_SIZE)
@@ -175,6 +93,50 @@ def drawButton():
 
     SCREEN.blit(NEW_GAME_SURF, NEW_GAME_RECT)
     SCREEN.blit(newGameTxtSurf, newGameTxtRect)
+
+def drawCells(surface:pg.Surface, rects: list[list[pg.Rect]]) -> None:
+    """Draw the (background) cells of the grid"""
+    for x in rects:
+        for y in x:
+            SCREEN.blit(surface, y)
+
+def renderSquares() -> None:
+    smallWriter = pg.font.Font(None, SQUARE_TXT_SIZE_SMALL_NUM)
+    largeWriter = pg.font.Font(None, SQUARE_TXT_SIZE_LARGE_NUM)
+    for s in allSquares:
+        squareVal = s.getNum() if s.isCombining is False else int(s.getNum()/2)
+        textSurf = None
+        if squareVal <= 4:
+            textSurf = smallWriter.render(str(squareVal), True, SQUARE_TXT_COLOR_SMALL_NUM)
+        elif squareVal < 1000:
+            textSurf = smallWriter.render(str(squareVal), True, SQUARE_TXT_COLOR_LARGE_NUM)
+        else:
+            textSurf = largeWriter.render(str(squareVal), True, SQUARE_TXT_COLOR_LARGE_NUM)
+
+        textRect = textSurf.get_rect(center=s.currRect.center)
+        SQUARE_SURFACE.fill(SQUARE_COLOR.get(squareVal))
+        SCREEN.blit(pg.transform.scale(SQUARE_SURFACE, s.currRect.size), s.currRect)
+        SCREEN.blit(textSurf, textRect)
+
+def gameOver() -> None:
+    writer = pg.font.Font(None, GAMEOVER_TXT_SIZE)
+    gameOverTxtSurf = writer.render('Game Over', True, (255, 255, 255))
+    gameOverTxtRect = gameOverTxtSurf.get_rect(center=GRID_RECT.center)
+
+    
+    SCREEN.blit(GAMEOVER_SURF, GRID_RECT)
+    SCREEN.blit(gameOverTxtSurf, gameOverTxtRect)
+
+#################### Functions for Game Logic ####################
+def isGameOver() -> bool:
+    for y in range(4):
+        for x in range(4):
+            if (occupiedCells.get(((x+1), y)) is not None and \
+                occupiedCells[(x+1, y)].getNum() == occupiedCells[(x, y)].getNum()) or \
+                    (occupiedCells.get((x, y+1)) is not None and \
+                        occupiedCells[(x, y+1)].getNum() == occupiedCells[(x, y)].getNum()):
+                return False
+    return True
 
 def moveSquares(dir:tuple[int, int]) -> bool:
     container = [[] for i in range(4)]
@@ -250,31 +212,71 @@ def updateSquares(hasMoved: bool) -> bool:
         return False
     return hasMoved
     
+def main():
+    # initialize the game
+    pg.init()
+    pg.font.init()
+    
+    global allSquares
+    global occupiedCells
+    allSquares = []
+    occupiedCells = {}
+    spawnSquare() 
+    spawnSquare()  
+    hasMoved = False
 
-def renderSquares() -> None:
-    smallWriter = pg.font.Font(None, SQUARE_TXT_SIZE_SMALL_NUM)
-    largeWriter = pg.font.Font(None, SQUARE_TXT_SIZE_LARGE_NUM)
-    for s in allSquares:
-        squareVal = s.getNum() if s.isCombining is False else int(s.getNum()/2)
-        textSurf = None
-        if squareVal <= 4:
-            textSurf = smallWriter.render(str(squareVal), True, SQUARE_TXT_COLOR_SMALL_NUM)
-        elif squareVal < 1000:
-            textSurf = smallWriter.render(str(squareVal), True, SQUARE_TXT_COLOR_LARGE_NUM)
+    global highScore
+    global currScore
+    highScore = 0
+    currScore = 0
+    with open('high_score.txt', 'a+') as f: # get local high score
+        f.seek(0)
+        m = f.read()
+        if m != '':
+            highScore = int(m)
         else:
-            textSurf = largeWriter.render(str(squareVal), True, SQUARE_TXT_COLOR_LARGE_NUM)
+            highScore = 0
 
-        textRect = textSurf.get_rect(center=s.currRect.center)
-        SQUARE_SURFACE.fill(SQUARE_COLOR.get(squareVal))
-        SCREEN.blit(pg.transform.scale(SQUARE_SURFACE, s.currRect.size), s.currRect)
-        SCREEN.blit(textSurf, textRect)
-         
-
-def drawCells(surface:pg.Surface, rects: list[list[pg.Rect]]) -> None:
-    """Draw the (background) cells of the grid"""
-    for x in rects:
-        for y in x:
-            SCREEN.blit(surface, y)
+    # main game loop
+    while True:
+        if currScore > highScore:
+            highScore = currScore
+        
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                with open('high_score.txt', 'w') as f:
+                    f.write(str(highScore)) 
+                sys.exit()
+            elif event.type == pg.KEYUP:
+                if event.key == pg.K_UP:
+                    hasMoved = moveSquares((0,-1))
+                elif event.key == pg.K_RIGHT:
+                    hasMoved = moveSquares((1, 0))
+                elif event.key == pg.K_LEFT:
+                    hasMoved = moveSquares((-1, 0))
+                elif event.key == pg.K_DOWN:
+                    hasMoved = moveSquares((0, 1))
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if NEW_GAME_RECT.collidepoint(pg.mouse.get_pos()):
+                    allSquares = []
+                    occupiedCells = {}
+                    spawnSquare() 
+                    spawnSquare()
+                    currScore = 0
+        # Draw background
+        SCREEN.fill(BGCOLOR) # reset screen
+        pg.draw.rect(SCREEN, GRID_BGCOLOR, GRID_RECT) # draw grid background
+        drawCells(CELL_SURFACE, CELL_RECTS) # draw each cells of grid
+        hasMoved = updateSquares(hasMoved)
+        drawScoreBox()
+        drawButton()
+        drawTitle()
+        
+        # draw squares
+        renderSquares()
+        if len(occupiedCells) == 16 and isGameOver():
+            gameOver()
+        pg.display.flip()
 
 if __name__ == '__main__':
     main()
